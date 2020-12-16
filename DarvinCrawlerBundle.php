2d80d6da9cf0128716dd8b318336dd5ac7f3b7f1
Igor Nikolaev <igor.sv.n@gmail.com>
2020-12-16 10:13:40 +0300

    Create bundle.

---
+++ DarvinCrawlerBundle.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2020, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\CrawlerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Crawler bundle
+ */
+class DarvinCrawlerBundle extends Bundle
+{
+
+}
